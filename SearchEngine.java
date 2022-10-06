b9f5799c4443bea7aa9f8dbfec7ff34e9d3336b1
vihsiao <vihsiao@ucsd.edu>
2022-10-05 17:13:28 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    String s = "";
+
+    String[] queries = new String[1];
+
+    public String handleRequest(URI url) {
+        if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            return "H";
+        }
+
+        return "";
+    }
+}
+
+public class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
